78def904a7414664f6d88b2ae8414366c890e7b2
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-12-01 18:23:45 +0500

    still background issues

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template, url_for
+from flask_assets import Environment, Bundle
 
 app = Flask(__name__)
+
+
+assets = Environment(app)
+
+
+scss = Bundle('scss/styles.scss', filters='pyscss', output='css/styles.css')
+assets.register('scss_all', scss)
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/about.html')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/membership.html')
+@app.route('/membership')
 def membership():
     return render_template('membership.html')
 
