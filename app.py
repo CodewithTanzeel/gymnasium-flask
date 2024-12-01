601a5156e5c297aa4432d047e9e3406cf0bb3ab6
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-12-01 18:10:36 +0500

    .

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask , render_template, url_for
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/about')
-def index():
+
+@app.route('/about.html')
+def about():
     return render_template('about.html')
-@app.route('/membership')
-def index():
+
+@app.route('/membership.html')
+def membership():
     return render_template('membership.html')
 
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(debug=False)
