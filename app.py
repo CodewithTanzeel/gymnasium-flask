f63592618010facec2c4ac43a786865b89a4065a
TanzeelAhmad <Tanzeelofficial@outlook.com>
2025-01-24 19:55:42 +0500

    .

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from flask import Flask, render_template, url_for
 from flask_assets import Environment, Bundle
-
 app = Flask(__name__)
-
-
 assets = Environment(app)
-
-
 scss = Bundle('scss/styles.scss', filters='pyscss', output='css/styles.css')
 assets.register('scss_all', scss)
-
 
 
 @app.route('/')
@@ -23,6 +17,7 @@ def about():
 @app.route('/membership')
 def membership():
     return render_template('membership.html')
+
 
 if __name__ == "__main__":
     app.run(debug=False)
